2b8d0790918befa98a8afb8eb8cf83a21124affc
Qodesmith <theqodesmith@gmail.com>
2024-09-25 14:04:26 -0400

    Updated work history

--- src/components/WorkHistory.jsx
+++ src/components/WorkHistory.jsx
@@ -7,11 +7,14 @@ export default function WorkHistory() {
       {/* INTRODUCTION */}
       <InfoCard title="Introduction">
         <div className="f-0-9em">
-          Hi, I'm Aaron and I'm all-things JavaScript! I'm interested in solving
-          problems with code, learning AI/ML, Hip-Hop, &amp; woodworking. I
-          maintain a few OSS libraries and occasionally{' '}
+          Hi, I'm Aaron and I'm all-things JavaScript! I have an obsession for
+          customer delight, pixels perfection, &amp; can't stop exploring and{' '}
+          <a href="https://beats.aaroncordova.dev" target="_blank">
+            building
+          </a>{' '}
+          things. I've occassionally{' '}
           <a href="https://youtu.be/7FRHuVNVCj4?t=661" className="royalblue">
-            give
+            given
           </a>{' '}
           <a
             href="https://www.youtube.com/watch?v=qDnOuV57TkY"
@@ -22,8 +25,8 @@ export default function WorkHistory() {
             href="https://www.youtube.com/watch?v=Cjt3q5hr-tw"
             className="royalblue">
             talks
-          </a>
-          .
+          </a>{' '}
+          😎
           <div className="mt4">
             Functional programming for the win.{' '}
             <span className="i">Readability &gt; Complexity</span>
@@ -34,9 +37,17 @@ export default function WorkHistory() {
       {/* EXPERIENCE */}
       <h1 className="ma0 pl16 pt16 ttu">Experience</h1>
       <WorkHistoryItem
+        company="Docker"
+        role="Senior Front End Engineer"
+        date="Dec 2023 - present">
+        <span className="i">Enabling devs to be productive.</span> Maintaining
+        and building features for Docker Desktop (Electron). Working on an
+        internal product bringing Docker to the cloud.
+      </WorkHistoryItem>
+      <WorkHistoryItem
         company="Coinbase"
         role="Senior Front End Engineer"
-        date="Oct 2021 - present">
+        date="Oct 2021 - Dec 2023">
         <span className="i">
           Harnessing the power of JavaScript to build a more open financial
           system.
@@ -47,8 +58,8 @@ export default function WorkHistory() {
         company="Facebook"
         role="Senior Front End Engineer"
         date="May 2019 - Oct 2021">
-        <span className="i">Make capacity easy to work with at scale.</span> I
-        design and build UI's for service owners to easily (&amp; happily)
+        <span className="i">Make capacity easy to work with at scale.</span>{' '}
+        Designed and built UI's for service owners to easily (&amp; happily)
         manage their backend service. Heavy work with custom forms, APIs, and
         data transformation.
       </WorkHistoryItem>
