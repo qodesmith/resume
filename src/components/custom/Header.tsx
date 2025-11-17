22c5c61d3cd106f9d709ab92b29420551134f1e3
Qodesmith <theqodesmith@gmail.com>
2025-11-17 08:05:50 -0500

    Added an intro to the header

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <header className="relative p-10">
+    <header className="relative pt-4 pr-4">
       <div className="-top-4 -left-4 absolute font-bold text-7xl leading-14">
         <div>AARON</div>
         <div>CORDOVA</div>
@@ -8,6 +8,24 @@ export function Header() {
       <p className="text-right font-light tracking-[4px]">
         JAVASCRIPT ENGINEER
       </p>
+      <div className="pr-1 text-right text-xs">
+        <p>
+          I'm Aaron! A fullstack JavaScript engineer obsessed with UX and peak{' '}
+          <span className="font-bold">performance</span>.
+        </p>
+        <p>
+          At companies like <span className="font-bold">Meta</span>,{' '}
+          <span className="font-bold">Coinbase</span>,{' '}
+          <span className="font-bold">Docker</span>, and{' '}
+          <span className="font-bold">Bloomberg</span>, I've architected
+          impactful
+        </p>
+        <p>
+          interfaces, scaling funnels by 30%, and creating UIs for complex LLM
+          systems.
+        </p>
+        <p className="italic">Readability &gt; Complexity</p>
+      </div>
     </header>
   )
 }
