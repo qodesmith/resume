094ae7c0e3483b9e82b7ba1c6fc68ce04a9ffc05
Qodesmith <theqodesmith@gmail.com>
2025-11-17 07:13:00 -0500

    Header created; using OpenSans google font

---
+++ src/components/custom/Header.tsx
@@ -0,0 +1,13 @@
+export function Header() {
+  return (
+    <header className="relative p-10">
+      <div className="-top-4 -left-4 absolute font-bold text-7xl leading-14">
+        <div>AARON</div>
+        <div>CORDOVA</div>
+      </div>
+      <p className="text-right font-light tracking-[4px]">
+        JAVASCRIPT ENGINEER
+      </p>
+    </header>
+  )
+}
--- src/components/custom/Resume.tsx
+++ src/components/custom/Resume.tsx
@@ -1,3 +1,9 @@
+import {Header} from './Header'
+
 export function Resume() {
-  return <div className="h-[11in] w-[8.5in] bg-blue-500">Resume</div>
+  return (
+    <div className="mx-auto my-16 h-[11in] w-[8.5in] overflow-hidden border font-opensans text-neutral-600">
+      <Header />
+    </div>
+  )
 }
